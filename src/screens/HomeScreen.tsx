3e08ce534032c81b230e9346f459095c669941de
ricardo <ricardo@ingeniabit.com>
2023-08-01 23:49:36 -0600

    Agregando axios y primera lista de pokemons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
+    const { top } = useSafeAreaInsets();
+    const { simplePokemonList, loadPokemons } = usePokemonPaginated();
+    
     return (
-        <View>
-            <Text>HOME SCREEN</Text>
-        </View>
+        <>
+            <Image
+                source={ require('../assets/pokebola.png') }
+                style={ styles.pokebolaBG }
+            />
+
+            <FlatList
+                data={ simplePokemonList }
+                keyExtractor={ (pokemon) => pokemon.id  }
+                showsVerticalScrollIndicator={ false }
+                renderItem={ ({ item }) => (
+                    <Image
+                        source={{ uri: item.picture }}
+                        style={{ 
+                            width: 100,
+                            height: 100
+                        }}
+                    />
+                )}
+                
+                //infinite scroll
+                onEndReached={ loadPokemons }
+                onEndReachedThreshold={ 0.4 }
+
+                ListFooterComponent={( 
+                    <ActivityIndicator 
+                        style={{ height: 100 }}
+                        size={ 20 }
+                        color='grey'
+                    /> 
+                )}
+            />
+
+            {/* <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20
+            }}>Pokedex</Text> */}
+        </>
     )
 }
